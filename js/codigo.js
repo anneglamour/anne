24a3d886ea82cb81bfe4061bfcd9937bc7b54119
anne Samaniego <anneadr2019@gmail.com>
2019-04-30 13:30:33 +0200

    corregido html

--- js/codigo.js
+++ js/codigo.js
@@ -18,4 +18,12 @@ document.write(" La hora actual es:" + d.getHours(),':', d.getMinutes()) ;
     } 
 
   
-// vamos a realizar el ejercicio juntos, 
+// vamos a realizar el ejercicio juntos,
+//los indices de los elementos array son los calores de getDay() 
+// para enviar el documento y trabajar con Git
+//primer paso hay que ir a git config user.name ""
+// suundo paso config user.email 
+// git add .
+//git commit - m" inicial comit"
+//git remote add github ( cambio)
+//git push
