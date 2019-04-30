a7d2f0b1fdb783558768d3f6b41812bf0cc68678
anne Samaniego <anneadr2019@gmail.com>
2019-04-30 12:40:58 +0200

    inicial comit

---
+++ js/codigo.js
@@ -0,0 +1,18 @@
+function copyanio()
+    lest anio= new Date(). getFullYear();
+    document.getElementById("copyanio").innerHTML = anio;
+var d=new Date();
+var dia=new Array(7);
+dia[0]="domingo";
+dia[1]="lunes";
+dia[2]="martes";
+dia[3]="miercoles";
+dia[4]="jueves";
+dia[5]="viernes";
+dia[6]="sabado";
+document.write(" Hoy es: " + dia[d.getDay()]);
+document.write(" La hora actual es:" + d.getHours(),':', d.getMinutes()) ;
+ 
+
+  
+// vamos a realizar el ejercicio juntos, 
